3fb4dd1083c786169fdeee967bb925e11057f7ee
Nishant Shastri <nishantshastri@Nishants-MacBook-Pro.local>
2019-09-24 20:49:54 -0400

    Script

---
+++ Assets/Apple.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    public static float bottomY = -20f;                                 // a
+
+    void Update()
+    {
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+            // Get a reference to the ApplePicker component of Main Camera
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();   // b
+            // Call the public AppleDestroyed() method of apScript
+            apScript.AppleDestroyed();                                      // b
+        }
+    }
+}
